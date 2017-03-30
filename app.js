878825797fc35c4b4864c7a3faba01ec6d030642
Ryan Phillips <rphillips4417@gmail.com>
2017-03-30 17:01:31 -0600

    basic html and routes

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const routes = require('./routes.js');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('./public/', { 'index': 'home.html' }));
+app.use(express.static('./public'));
 app.use(bodyParser.json());
 app.use('/', routes);
 
--- routes.js
+++ routes.js
@@ -11,7 +11,18 @@ const db = low('db/movies.json', {
 //
 // ^^^^^^^^^^Create Movie^^^^^^^^^^
 
-// router.post()
+router.post('/movies', (req, res) => {
+ req.body.id = parseInt(req.body.id)
+ db.get('movies')
+ .push(req.body)
+ .write()
+ .then(result => {
+   res.status(201).send(result)
+ })
+ .catch(error => {
+   console.log(error);
+ })
+})
 
 
 
@@ -20,18 +31,23 @@ const db = low('db/movies.json', {
 // ****************** READ ******************
 //
 // ^^^^^^^^^^Read Movie^^^^^^^^^^
+router.get('/movies', (req, res) => {
+  // Get route has to find movies
+  const movies = db.get('movies')
+  // send movies back as response
+  res.send(movies)
 
+})
 
 
 
 // ^^^^^^^^^^Read One Movie^^^^^^^^^^
 
-
-
-
-
-
-
+router.get('/movies/:id', (req ,res) => {
+    const movieID = parseInt(req.params.id)
+    const singleMovie = db.get('movies').find({id: movieID})
+    res.send(singleMovie)
+})
 
 // ****************** Update ******************
 //
